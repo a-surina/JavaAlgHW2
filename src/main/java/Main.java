1db83082a3b11a1ba76073e1856e3fd5903dc8f4
nastenka-rom <nastenka-rom@yandex.ru>
2021-03-16 18:18:28 +0300

    Search methods done

---
+++ src/main/java/Main.java
@@ -0,0 +1,95 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Задание 2.1: реализуем массив
+        int[] arrayToCopyAndCompare;
+        arrayToCopyAndCompare = new int[1000];
+
+        Random rand = new Random();
+        for (int i = 0; i<1000; i++) {
+            arrayToCopyAndCompare[i] = rand.nextInt(1000);
+        }
+
+        //Задание 2.1: базовые операции класса Arrays, время выполнения
+        long startTimeString = System.nanoTime();
+        String arrayToSortAsString = Arrays.toString(arrayToCopyAndCompare);
+        double elapsedNanosString = (double) (System.nanoTime() - startTimeString)/1000000;
+        System.out.println("2.1.1. Printed array: " + arrayToSortAsString);
+        System.out.println("\tTime elapsed on 'Arrays.toString': " + String.valueOf(elapsedNanosString) + " milliseconds\n");
+
+        long startTimeCopy = System.nanoTime();
+        int[] arrayCopy = Arrays.copyOf(arrayToCopyAndCompare, arrayToCopyAndCompare.length);
+        double elapsedNanosCopy = (double) (System.nanoTime() - startTimeCopy)/1000000;
+        System.out.println("2.1.2. Copied array: " + Arrays.toString(arrayCopy));
+        System.out.println("\tTime elapsed on 'Arrays.copyOf': " + String.valueOf(elapsedNanosCopy) + " milliseconds\n");
+
+        long startTimeCompare = System.nanoTime();
+        int arrayCompare = Arrays.compare(arrayToCopyAndCompare, arrayCopy);
+        double elapsedNanosCompare = (double) (System.nanoTime() - startTimeCompare)/1000000;
+        System.out.println("2.1.3. Arrays are equal: " + String.valueOf(arrayCompare));
+        System.out.println("\tTime elapsed on 'Arrays.compare': " + String.valueOf(elapsedNanosCompare) + " milliseconds\n");
+
+        //Задание 2.2: линейный поиск (поиск реализован в классе SearchManager)
+        SearchManager finder = new SearchManager();
+        Arrays.sort(arrayToCopyAndCompare); //Сортируем для наглядности
+
+        long startTimeLinearSearch = System.nanoTime();
+        int indexL = finder.linearSearch(arrayToCopyAndCompare, 36);
+        double elapsedNanosLinearSearch = (double) (System.nanoTime() - startTimeLinearSearch)/1000000;
+        System.out.println("2.2.1. Linear Search found element at index: " + String.valueOf(indexL));
+        System.out.println("\tTime elapsed: " + String.valueOf(elapsedNanosLinearSearch) + " milliseconds\n");
+
+        //Задание 2.2: двоичный поиск поиск (поиск реализован в классе SearchManager)
+        long startTimeBinarySearch = System.nanoTime();
+        int indexB = finder.binarySearch(arrayToCopyAndCompare, 36);
+        double elapsedNanosBinarySearch = (double) (System.nanoTime() - startTimeBinarySearch)/1000000;
+        System.out.println("2.2.2. Binary Search found element at index: " + String.valueOf(indexB));
+        System.out.println("\tTime elapsed: " + String.valueOf(elapsedNanosBinarySearch) + " milliseconds\n");
+
+        //Задание 2.3: массив размером 400 элементов, сортировка sort() и время выполнения
+        int[] arrayToSort;
+        arrayToSort = new int[400];
+        for (int i = 0; i<400; i++) {
+            arrayToSort[i] = rand.nextInt(1000);
+        }
+        System.out.println("2.3.1. Unsorted array: " + Arrays.toString(arrayToSort));
+
+        int[] arrayToSortCopy1 = Arrays.copyOf(arrayToSort, arrayToSort.length);
+        int[] arrayToSortCopy2 = Arrays.copyOf(arrayToSort, arrayToSort.length);
+        int[] arrayToSortCopy3 = Arrays.copyOf(arrayToSort, arrayToSort.length);
+
+        long startTimeSort= System.nanoTime();
+        Arrays.sort(arrayToSort);
+        double elapsedNanosSort = (double) (System.nanoTime() - startTimeSort)/1000000;
+        System.out.println("2.3.2. Array sorted by 'sort()': " + Arrays.toString(arrayToSort));
+        System.out.println("\tTime elapsed: " + String.valueOf(elapsedNanosSort) + " milliseconds\n");
+
+        //Задание 2.4: сортировка пузырьком (сортировка реализована в классе SortManager)
+        SortManager sorter = new SortManager();
+        System.out.println("2.4.1. Unsorted array copy1: " + Arrays.toString(arrayToSortCopy1));
+        long startTimeSortBubble= System.nanoTime();
+        sorter.bubbleSort(arrayToSortCopy1);
+        double elapsedNanosSortBubble = (double) (System.nanoTime() - startTimeSortBubble)/1000000;
+        System.out.println("2.4.2. Array sorted by bubble sort: " + Arrays.toString(arrayToSortCopy1));
+        System.out.println("\tTime elapsed: " + String.valueOf(elapsedNanosSortBubble) + " milliseconds\n");
+
+        //Задание 2.5: сортировка методом выбора (сортировка реализована в классе SortManager)
+        System.out.println("2.5.1. Unsorted array copy2: " + Arrays.toString(arrayToSortCopy2));
+        long startTimeSortSelection= System.nanoTime();
+        sorter.selectionSort(arrayToSortCopy2);
+        double elapsedNanosSortSelection = (double) (System.nanoTime() - startTimeSortSelection)/1000000;
+        System.out.println("2.5.2. Array sorted by selection sort': " + Arrays.toString(arrayToSortCopy2));
+        System.out.println("\tTime elapsed: " + String.valueOf(elapsedNanosSortSelection) + " milliseconds\n");
+
+        //Задание 2.6: сортировка методом вставки (сортировка реализована в классе SortManager)
+        System.out.println("2.6.1. Unsorted array copy3: " + Arrays.toString(arrayToSortCopy3));
+        long startTimeSortInsertion= System.nanoTime();
+        sorter.insertionSort(arrayToSortCopy2);
+        double elapsedNanosSortInsertion = (double) (System.nanoTime() - startTimeSortInsertion)/1000000;
+        System.out.println("2.6.2. Array sorted by insertion sort" + Arrays.toString(arrayToSortCopy3));
+        System.out.println("\tTime elapsed: " + String.valueOf(elapsedNanosSortInsertion) + " milliseconds\n");
+    }
+}
---
+++ src/main/java/SortManager.java
@@ -0,0 +1,17 @@
+public class SortManager {
+
+    public SortManager() {}
+
+    //Сортировка пузырьком
+    public int bubbleSort(int[] arr) {
+        return -1;
+    }
+    //Сортировка методом выбора
+    public int selectionSort(int[] arr) {
+        return -1;
+    }
+    //Сортировка методом вставки
+    public int insertionSort(int[] arr) {
+        return -1;
+    }
+}
